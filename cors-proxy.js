ce87f0cf62fb666ad7346d584880fd6671fe9351
Alex <tischlinger.alex@gmail.com>
2017-10-11 13:09:51 +0200

    done, now functioning - changed proxy-port back to 8080, because reasons - disabled strict in tsconfig, because of errors in building

--- cors-proxy.js
+++ cors-proxy.js
@@ -2,7 +2,7 @@ var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
     originWhitelist: []
     // using port 8081, 8080 does not work
-    // probably a proxy server from windows
-}).listen(8081, '127.0.0.1', function() {
+    // probably a proxy server from windows or school network
+}).listen(8080, '127.0.0.1', function() {
     console.log('Running CORS Anywhere');
 });
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Run our script once the browser has finished loading all resources
 // and has created the DOM. For details see
 // https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers/onload
-window.onload = function () {
+window.onload = () => {
     // Get relevant elements in DOM by id. Want to know which browsers support
     // getElementById? Check http://caniuse.com/#search=getElementbyid
-    var resultBody = document.getElementById('resultBody'), result = document.getElementById('result'), loading = document.getElementById('loading'), filter = document.getElementById('filter'), search = document.getElementById('search');
+    const resultBody = document.getElementById('resultBody'), result = document.getElementById('result'), loading = document.getElementById('loading'), filter = document.getElementById('filter'), search = document.getElementById('search');
     // Helper function to load tracks
     function loadTracks() {
         // Remove all Tracks from DOM
@@ -13,22 +13,22 @@ window.onload = function () {
         }
         // Build XHR. Need details about XMLHttpRequest? Check
         // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
-        var xhr = new XMLHttpRequest();
-        xhr.addEventListener('load', function () {
+        const xhr = new XMLHttpRequest();
+        xhr.addEventListener('load', () => {
             // Parse result and extract relevant track data
-            var deezerReponse = JSON.parse(xhr.response).data;
-            var jsonResult = deezerReponse.map(function (d) {
+            const deezerReponse = JSON.parse(xhr.response).data;
+            const jsonResult = deezerReponse.map(d => {
                 return { title: d.title, artist: d.artist.name };
             });
             // Iterate over result and create track results in DOM
-            jsonResult.forEach(function (track) {
+            jsonResult.forEach(track => {
                 // Build DOM using JavaScript functions. Note that you could also use
                 // "innerHtml" instead.
-                var tr = document.createElement('tr');
-                var title = document.createElement('td');
+                const tr = document.createElement('tr');
+                const title = document.createElement('td');
                 title.appendChild(document.createTextNode(track.title));
                 tr.appendChild(title);
-                var artist = document.createElement('td');
+                const artist = document.createElement('td');
                 artist.appendChild(document.createTextNode(track.artist));
                 tr.appendChild(artist);
                 resultBody.appendChild(tr);
@@ -39,7 +39,7 @@ window.onload = function () {
         });
         // Build URL
         // Quiz: Why do we use a proxy instead of directly accessing Deezer's search API?
-        var url = 'http://localhost:8080/https://api.deezer.com/search?q=' + filter.value;
+        let url = 'http://localhost:8080/https://api.deezer.com/search?q=' + filter.value;
         // Send request
         xhr.open('GET', url);
         xhr.setRequestHeader('X-Requested-With', '.');
@@ -49,7 +49,7 @@ window.onload = function () {
         loading.hidden = false;
     }
     // Next page
-    search.onclick = function () {
+    search.onclick = () => {
         loadTracks();
     };
 };
